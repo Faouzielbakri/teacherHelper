9b7147760e242ba32f52358f74145306cf477f69
Faouzi EL BAKRI <faouzielbakri@gmail.com>
2024-02-14 21:05:16 +0100

    docker compose and dockerfile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // mine worked fine without this line
   reactStrictMode: false,
   images: {
     remotePatterns: [
